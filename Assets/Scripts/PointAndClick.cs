8db315c2e4444c6a6061acd27caaa8160a22027d
Ricky Chon <rickychon99@gmail.com>
2023-07-08 02:18:50 -0400

    Make `PointAndClick` abstract & add wiping script
    `PointAndClick` can now be extended for feature specific point-and-click
    logic. All pointer handlers can be overriden or extended to provide
    additional logic.

--- Assets/Scripts/PointAndClick.cs
+++ Assets/Scripts/PointAndClick.cs
@@ -7,7 +7,7 @@ using UnityEngine.EventSystems;
 /// Will listen for Pointer events for the attached GameObject.
 /// GameObject must be a UI component (Image, RawImage, Text).
 /// </summary>
-public class PointAndClick : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler,
+public abstract class PointAndClick : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler,
     IPointerUpHandler, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     // TODO: Make class abstract and extendable for different types of game actions
@@ -22,7 +22,7 @@ public class PointAndClick : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     {
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+    public virtual void OnPointerEnter(PointerEventData eventData)
     {
         Debug.Log("Entered " + gameObject.name);
 
@@ -36,36 +36,36 @@ public class PointAndClick : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         // Change cursor to normal sprite
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    public virtual void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("Down " + gameObject.name);
     }
 
-    public void OnPointerUp(PointerEventData eventData)
+    public virtual void OnPointerUp(PointerEventData eventData)
     {
         Debug.Log("Up " + gameObject.name);
     }
 
-    public void OnPointerClick(PointerEventData eventData)
+    public virtual void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Clicked " + eventData.pointerCurrentRaycast.gameObject.name);
 
         // Perform some game action depending on what GameObject it is
     }
 
-    public void OnBeginDrag(PointerEventData eventData)
+    public virtual void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Begin drag " + gameObject.name);
     }
 
-    public void OnDrag(PointerEventData eventData)
+    public virtual void OnDrag(PointerEventData eventData)
     {
         Debug.Log("Dragging " + gameObject.name);
 
         // Game logic related to dragging
     }
 
-    public void OnEndDrag(PointerEventData eventData)
+    public virtual void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("End drag " + gameObject.name);
     }
