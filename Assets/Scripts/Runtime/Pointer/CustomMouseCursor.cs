01fa513622d66c00c025eaf8426ecbeb771d0db5
Ricky Chon <rickychon99@gmail.com>
2023-07-09 00:02:22 -0400

    Add animated custom cursor script (#18)
    * Animated Manager
    
    * Move cursor sprites to Assets/Sprites
    
    * Refactor `CustomMouseCursor` for best practices
    
    * Update CleanFloorTest scene
    
    ---------
    
    Co-authored-by: Andy Wu <www.flamer1959@hotmail.com>

--- Assets/Scripts/Runtime/Pointer/CustomMouseCursor.cs
+++ Assets/Scripts/Runtime/Pointer/CustomMouseCursor.cs
@@ -1,26 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Peebo.Runtime.Pointer
 {
-    public class CustomMouseCursor : MonoBehaviour
+    public class CustomMouseCursor : PointAndClick
     {
-        public Texture2D mouseCursor;
+        [Tooltip("Sprites to use for cursor OnPointerEnter")]
+        [SerializeField] public Texture2D[] pointerCursors;
+        [Tooltip("Sprites to use for default cursor")]
+        [SerializeField] public Texture2D[] defaultCursors;
+        [Tooltip("Coordinates for cursor click position (default is top left)")]
+        [SerializeField] public Vector2 hotSpot = new(0, 0);
 
-        Vector2 hotSpot = new Vector2(0, 0);
-        CursorMode cursorMode = CursorMode.Auto;
+        private IEnumerator _animatePointerCursorCo;
+        private IEnumerator _animateDefaultCursorCo;
+        private int _currentFrame = 0;
 
         // Start is called before the first frame update
         void Start()
         {
-            Cursor.SetCursor(mouseCursor, hotSpot, cursorMode);
+            _animatePointerCursorCo = AnimateCursor(pointerCursors);
+            _animateDefaultCursorCo = AnimateCursor(defaultCursors);
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+        }
+
+        IEnumerator AnimateCursor(Texture2D[] cursors) {
+            while(cursors.Length > 0) {
+                _currentFrame = (_currentFrame + 1) % cursors.Length;
+                Cursor.SetCursor(cursors[_currentFrame], hotSpot, CursorMode.ForceSoftware);
+                yield return new WaitForSeconds(0.5f);
+                Debug.Log("CUSTOM CURSOR");
+            }
+        }
+
+        public override void OnPointerExit(PointerEventData eventData)
+        {
+            _currentFrame = 0;
+
+            StopCoroutine(_animatePointerCursorCo);
+            StartCoroutine(_animateDefaultCursorCo);
+        }
+
+        public override void OnPointerEnter(PointerEventData eventData)
+        {
+            StopCoroutine(_animateDefaultCursorCo);
+            StartCoroutine(_animatePointerCursorCo);
         }
     }
 }
