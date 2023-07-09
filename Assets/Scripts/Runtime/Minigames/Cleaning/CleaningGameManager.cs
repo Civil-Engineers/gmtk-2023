de1af4c04c7e15ef460f4aa7b8b763c0b350c6cc
Ricky Chon <rickychon99@gmail.com>
2023-07-08 22:44:02 -0400

    Outline objects to clean on hover (#19)

---
+++ Assets/Scripts/Runtime/Minigames/Cleaning/CleaningGameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Peebo.Runtime.Minigames.Cleaning
+{
+    /// <summary>
+    /// Manages game state for cleaning minigame on scene load.
+    /// 
+    /// It should keep track of all gameObjects that have the StainWipeHandler script.
+    /// When all stains are wiped clean, switch scenes using a Scene Manager.
+    /// </summary>
+    public class CleaningGameManager : MonoBehaviour
+    {
+        [SerializeField] public GameObject[] stains;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
